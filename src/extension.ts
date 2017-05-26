91934b5ecd53559d019fc5a485af423dcab7ae57
Albin Theander <Albin.Theander@jayway.com>
2017-05-26 12:13:47 +0200

    First commit. Seems to work. :-)

---
+++ src/extension.ts
@@ -0,0 +1,68 @@
+'use strict';
+import { Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, window } from 'vscode';
+import RollingArray from './rollingqueue';
+
+export function activate(context: ExtensionContext) {
+    let typingSpeed = new TypingSpeed();
+    let controller = new TypingSpeedController(typingSpeed);
+
+    context.subscriptions.push(controller);
+    context.subscriptions.push(typingSpeed);
+}
+
+class TypingSpeed {
+    private _keystrokes: RollingArray = new RollingArray(100);
+    private _statusBarItem: StatusBarItem;
+
+    public updateSpeed() {
+
+        if (!this._statusBarItem) {
+            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+
+        const now = Date.now();
+        this._keystrokes.add(now);
+
+        let cpm = 0;
+        const count = this._keystrokes.size();
+        if (count > 1) {
+            const firstTime = this._keystrokes.first();
+            cpm = count * 60000 / (now - firstTime);
+        }
+
+        this._statusBarItem.text = `$(keyboard) ${cpm.toFixed(1)} cps`;
+        this._statusBarItem.show();
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+class TypingSpeedController {
+    private _typingSpeed: TypingSpeed;
+    private _disposable: Disposable;
+
+    constructor(typingSpeed: TypingSpeed) {
+        this._typingSpeed = typingSpeed;
+
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+
+        this._typingSpeed.updateSpeed();
+
+        this._disposable = Disposable.from(...subscriptions);
+    }
+
+    dispose() {
+        this._disposable.dispose();
+    }
+
+    private _onEvent() {
+        this._typingSpeed.updateSpeed();
+    }
+}
---
+++ src/rollingqueue.ts
@@ -0,0 +1,34 @@
+export default class RollingArray {
+    private _data = [];
+    private _size: number;
+    private _first: number = 0;
+    private _last: number = -1;
+
+    constructor(size) {
+        this._size = size;
+    }
+
+    add(x) {
+        this._last += 1;
+        if (this._last === this._size) this._last = 0;
+
+        this._data[this._last] = x;
+
+        if (this._data.length === this._size) {
+            this._first += 1;
+            if (this._first === this._size) this._first = 0;
+        }
+    }
+
+    size(): number {
+        return this._data.length;
+    }
+
+    first() {
+        return this._data[this._first];
+    }
+
+    last() {
+        return this._data[this._last];
+    }
+}
